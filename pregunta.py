5c64c13515dcbabb97a29f5b7785970beacf7149
johanUNAL753 <jmarincr@unal.edu.co>
2023-10-17 12:42:39 -0500

    Laboratorio Solicitudes Credito
    Envio todos los puntos

--- pregunta.py
+++ pregunta.py
@@ -1,20 +1,17 @@
-"""
-Limpieza de datos usando Pandas
------------------------------------------------------------------------------------------
-
-Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizada 
-correctamente. Tenga en cuenta datos faltantes y duplicados.
-
-"""
 import pandas as pd
 
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = pd.read_csv("solicitudes_credito.csv", sep=";",index_col=0)
 
-    #
-    # Inserte su código aquí
-    #
+    df.dropna(inplace=True)
+
+    df = df.apply(lambda x: x.astype(str).str.replace("_","-").str.replace("-"," ").str.replace("$","").str.replace(",",""))
+    df = df.apply(lambda x: x.str.lower())
+    df.fecha_de_beneficio = pd.to_datetime(df["fecha_de_beneficio"],format='mixed', dayfirst = True)
+    df.monto_del_credito = df.monto_del_credito.astype(float)
+    
+    df.drop_duplicates(inplace=True)
 
     return df
